d39a1c1c4cf04987a6c85b35e28351b7998b2f17
Viacheslav-Yurushev <108462275+ViacheslavYUR@users.noreply.github.com>
2022-11-13 12:08:39 +0200

    task1-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsRef = galleryItems;
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItemsRef);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItemsMarkup(img) {
+  return img
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg" onclick="return false" rel="noreferrer noopener">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+          
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", imgGalleryclick);
+
+function imgGalleryclick(e) {
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const parentImgEl = e.target.closest(".gallery__link");
+
+  parentImgEl.href = e.target.dataset.source;
+  console.log(parentImgEl);
+
+  openModalWindow(parentImgEl);
+}
+
+function openModalWindow(parentImgEl) {
+  const instance = basicLightbox.create(`
+    <img src="${parentImgEl.href}" width="800" height="600">
+`);
+
+  instance.show();
+}
+//   console.log(markup);
