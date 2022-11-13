6d9fdd93cd850b16b3ac2e8f98d535a0dd279570
Viacheslav-Yurushev <108462275+ViacheslavYUR@users.noreply.github.com>
2022-11-13 12:54:36 +0200

    task2-modalDone

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryItemsRef = galleryItems;
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItemsRef);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItemsMarkup(img) {
+  return img
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__item" href="${original}" onclick="return false" rel="noreferrer noopener">
+   <img class="gallery__image"  src="${preview}" alt="${description}" />
+ </a>;
+    </li>`;
+    })
+    .join("");
+}
+
+let ligthbox = new SimpleLightbox(".gallery__item");
